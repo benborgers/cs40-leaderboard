ef9cc5b7ffc2ebfde6c101a5e839861980f10f00
Ben Borgers <borgersbenjamin@gmail.com>
2025-04-09 02:34:35 -0400

    Styling

--- build.js
+++ build.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const path = require("path");
 
 const PLACEHOLDER = "{{ PLACEHOLDER }}";
+const ONE_DAY_IN_MS = 86400000;
 
 const calculateScore = (run) => {
   return run.midmark_time + run.sandmark_time;
@@ -48,6 +49,19 @@ const pythonTimestampToDate = (timestamp) => {
   return new Date(year, month - 1, day, hour, minute, second);
 };
 
+const latestRun = Object.values(bestRunsByID).reduce((latest, current) => {
+  const currentTime = pythonTimestampToDate(current.timestamp).getTime();
+  const latestTime = pythonTimestampToDate(latest.timestamp).getTime();
+  return currentTime > latestTime ? current : latest;
+});
+
+const placeString = (index) => {
+  if (index === 0) return "🥇";
+  if (index === 1) return "🥈";
+  if (index === 2) return "🥉";
+  return "#" + (index + 1);
+}
+
 if (!fs.existsSync("_site")) {
   fs.mkdirSync("_site");
 }
@@ -57,32 +71,46 @@ fs.writeFileSync(
   fs.readFileSync(path.join(__dirname, "template.html"), "utf8").replace(
     PLACEHOLDER,
     sortedRuns
-      .map(
-        (run, index) => `
+      .map((run, index) => {
+        const place = placeString(index);
+        const maxPlaceString = placeString(sortedRuns.length - 1);
+        const smallIndentSpaces = "&nbsp;".repeat(
+          maxPlaceString.length - place.length
+        );
+        const bigIndentSpaces = "&nbsp;".repeat(maxPlaceString.length + 1);
+        const highlightAsLatest =
+          run.id === latestRun.id &&
+          Date.now() - pythonTimestampToDate(run.timestamp).getTime() <
+          ONE_DAY_IN_MS;
+
+        return `
 <div>
   <div>
-    ${index + 1}
-  </div>
-  <div>
     <p>
+      ${smallIndentSpaces}${place}:
       ${run.name}
       ${run.subtitle ? `(${run.subtitle})` : ""}
     </p>
     <p>
-      <span>Midmark: ${round(run.midmark_time, 3)}s</span>
-      <span>Sandmark: ${round(run.sandmark_time, 3)}s</span>
+      ${bigIndentSpaces}
+      midmark: ${round(run.midmark_time, 3)}s
+      /
+      sandmark: ${round(run.sandmark_time, 3)}s
     </p>
   </div>
-  <p>
+  <p class="${highlightAsLatest ? "text-red-600/75" : "text-gray-400"}">
+    ${bigIndentSpaces}
     ${run.timestamp_override
             ? run.timestamp_override
             : `<span data-timestamp="${pythonTimestampToDate(
               run.timestamp
-            ).getTime()}"></span>`
+            ).getTime()}"></span>
+            ${highlightAsLatest ? `[latest]` : ""}
+            `
           }
   </p>
-</div>`
-      )
+</div>`;
+      })
       .join("\n")
   )
 );
